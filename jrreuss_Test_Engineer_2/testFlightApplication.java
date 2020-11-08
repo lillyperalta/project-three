3fbf2117edf7eb9c40c33d07c4df39f78b27a831
joereuss12 <71942931+joereuss12@users.noreply.github.com>
2020-11-08 15:24:12 -0600

    Add files via upload

--- jrreuss_Test_Engineer_2/testFlightApplication.java
+++ jrreuss_Test_Engineer_2/testFlightApplication.java
@@ -1,1 +1,42 @@
-
+// --== CS400 File Header Information ==--
+// Name: Joseph Reuss
+// Email: jrreuss@wisc.edu
+// Team: KA
+// TA: Sid
+// Lecturer: Gary Dahl
+// Notes to Grader: <optional extra notes>
+import org.junit.jupiter.api.Test;
+public class testFlightApplication {
+  
+  @Test
+  public void testAddAirport() {
+    
+  }
+  
+  @Test
+  public void testAddFlight() {
+    
+  }
+  
+  @Test
+  public void testAirportCount() {
+    
+  }
+  
+  @Test
+  public void testFlightCount() {
+    
+  }
+  
+  @Test
+  public void testShortestPath() {
+    
+  }
+  
+  @Test
+  public void testFlightDuration() {
+    
+  }
+  
+  
+}
